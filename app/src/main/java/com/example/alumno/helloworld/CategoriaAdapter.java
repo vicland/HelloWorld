9aa8a88f6f188ab808d4fcef35c18075d4ce9436
vicland <lanchares93@gmail.com>
2015-03-30 17:30:03 +0200

    Version1

---
+++ app/src/main/java/com/example/alumno/helloworld/CategoriaAdapter.java
@@ -0,0 +1,80 @@
+package com.example.alumno.helloworld;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+/**
+ * Created by damonfor on 26/03/2015.
+ */
+public class CategoriaAdapter extends BaseAdapter {
+    private Context context;
+    private final String[] mobileValues;
+
+    public CategoriaAdapter(Context context, String[] mobileValues) {
+        this.context = context;
+        this.mobileValues = mobileValues;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View gridView;
+
+        if (convertView == null) {
+
+            gridView = new View(context);
+
+            // get layout from mobile.xml
+            gridView = inflater.inflate(R.layout.categoria, null);
+
+            // set value into textview
+            TextView textView = (TextView) gridView
+                    .findViewById(R.id.grid_item_label);
+            textView.setText(mobileValues[position]);
+
+            // set image based on selected text
+            ImageView imageView = (ImageView) gridView
+                    .findViewById(R.id.grid_item_image);
+
+            String mobile = mobileValues[position];
+
+            if (mobile.equals("Windows")) {
+                imageView.setImageResource(R.drawable.elegirdeporte);
+            } else if (mobile.equals("iOS")) {
+                imageView.setImageResource(R.drawable.fondo_baloncesto);
+            } else if (mobile.equals("Blackberry")) {
+                imageView.setImageResource(R.drawable.fondo_futbol);
+            } else {
+                imageView.setImageResource(R.drawable.fondo_baloncesto);
+            }
+
+        } else {
+            gridView = (View) convertView;
+        }
+
+        return gridView;
+    }
+
+    @Override
+    public int getCount() {
+        return mobileValues.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+}
